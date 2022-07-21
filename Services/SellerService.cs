b7d248fa9e4729d4cd4546759feeb19b2aef4720
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-07-21 16:58:53 -0300

    SellerService and basic find all

---
+++ Services/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
