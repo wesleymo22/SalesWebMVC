8894dd64706c3c69f537b5142fb5ed1d87e717c7
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-07-27 04:12:36 -0300

    Update Seller and custom service exception

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,23 @@ namespace SalesWebMVC.Services
             _context.SaveChanges();
         }
 
-        
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not Found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
