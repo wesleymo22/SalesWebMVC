29472cc0cc7b787fc3a1ced8b865dd18e91deeb2
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-07-30 03:39:18 -0300

    Validation

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -38,6 +38,10 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(seller);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -104,6 +108,10 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(seller);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
